906e790f3ae45109b705178eed4e16e254dab9e5
Joao Pilger <joao.pilger@glim.com.br>
2025-06-07 13:19:28 -0300

    v15

--- src/components/MusicReleases.tsx
+++ src/components/MusicReleases.tsx
@@ -11,72 +11,88 @@ const MusicReleases: React.FC = () => {
   const releases: MusicRelease[] = [
     {
       title: 'PANIN ft. ' + 
-             'Dj Theuzz',
-      type: 'single',
+             'DJ THEUZZ',
+      type: 'SINGLE',
       year: 2025,
-      link: 'https://soundcloud.com/rhonaz/panin'
+      link: 'https://open.spotify.com/intl-pt/track/0GjUlF6WWawO1New6waV9A?si=1fff65975dd14f06'
     },
     {
       title: 'DICOPANA ft. ' + 
              'LOFIHOUSEBOY',
-      type: 'single',
+      type: 'SINGLE',
       year: 2025,
-      link: 'https://soundcloud.com/artigo016/rhonaz-kera?in=rhonaz/sets/kera-artigo016-rhonaz'
+      link: 'https://open.spotify.com/album/6N0BFmIknyeGKDlMfEaxwV?si=_ysJUbF3Qky4Xe7mbD1hyA'
     },
     {
-      title: 'KERA ft. Artigo016',
-      type: 'single',
+      title: 'KERA ft. ARTIGO016',
+      type: 'SINGLE',
       year: 2025,
       link: 'https://soundcloud.com/artigo016/rhonaz-kera?in=rhonaz/sets/kera-artigo016-rhonaz'
     },
     {
       title: 'KIT KATRINA',
-      type: 'single',
+      type: 'SINGLE',
       year: 2025,
-      link: 'https://soundcloud.com/rhonaz/kit-katrina-dj-rhonaz'
+      link: 'https://open.spotify.com/album/0unPFY4llwWh3oI2UYpZMu?si=YIbnBgKESjGgLobFVvsShg'
     },
     {
-      title: 'Desastre Ritmo',
-      type: 'album',
-      year: 2024,
-      link: 'https://open.spotify.com/track/...'
-    },
-    {
-      title: 'Ritmando Laje',
-      type: 'set',
-      year: 2024,
-      link: 'https://open.spotify.com/track/...'
-    },
-    {
-      title: 'Fikadi ft. Zero',
-      type: 'single',
-      year: 2024,
-      link: 'https://open.spotify.com/track/...'
-    },
-    {
-      title: 'Pompoarismo',
-      type: 'remix',
+      title: 'FIK4DI ft. ZERO',
+      type: 'SINGLE',
       year: 2025,
-      link: 'https://open.spotify.com/track/...'
-    }
+      link: 'https://open.spotify.com/album/1OeGvIAHdE2f0GhnJCMbKm?si=eKXYWgxzQ_iWGpIGxuIpYQ'
+    },
+    {
+      title: 'POMPOARISMO',
+      type: 'REMIX',
+      year: 2025,
+      link: 'https://soundcloud.com/rhonaz/mu540-bia-soull-pompoarismo-dj-rhonaz-remix'
+    },
+    {
+      title: 'DESASTRE RITMO',
+      type: 'ALBUM',
+      year: 2024,
+      link: 'https://open.spotify.com/album/1izHa5aDBA8rHLoC6UdP2X?si=W7tl_1wDSOeO2vLtGyvRMg'
+    },
+    {
+      title: 'RITMANDO NA LAJE',
+      type: 'LIVE SET',
+      year: 2024,
+      link: 'https://www.youtube.com/watch?v=StRiM_XKpRI&t=1352s'
+    },
+    {
+      title: 'TAKA FOGO EM KIKSILVER',
+      type: 'REMIX',
+      year: 2024,
+      link: 'https://open.spotify.com/album/0r1GI7ZUNc9QNNnBJEdsMY?si=tluQ6rubShydM96h1F5_nw'
+    },
   ];
 
   return (
     <div className="y2k-content-area-musicas">
-      <pre className="terminal-style">
-        <p>drops</p>
-        <p>-----------------------------------------------------------------------</p>
-        <p>  NOME                       TIPO            ANO       LINK   </p>
-        <p>-----------------------------------------------------------------------</p>
-        {releases.map((release, index) => (
-          <p key={index}>
-            {`  ${release.title.padEnd(26)} ${release.type.padEnd(15)} ${release.year}      `}
-            <a target="_blank" rel="noopener noreferrer" href={release.link}> 📀 </a>
-          </p>
-        ))}
-        <p>                                                       </p>
-        <p>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>
-      </pre>
+      <div>
+        <pre className="terminal-style">
+          <p style={{marginTop: 12 }}> últimos drops</p>
+          <p>-----------------------------------------------------------------------</p>
+          <p>  NOME                          TIPO            ANO     </p>
+          <p>-----------------------------------------------------------------------</p>
+          {releases.map((release, index) => (
+            <a target="_blank" rel="noopener noreferrer" href={release.link}><p key={index}>
+              {`  ${release.title.padEnd(28)} ${release.type.padEnd(16)} ${release.year}      `}
+            </p>
+            </a>
+          ))}
+          <p>                                                       </p>
+          <p>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>
+        </pre>
+      </div>
+      <div style={{margin: 12}}>
+        <h2>playlist singles</h2>
+        <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1680309441&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+      </div>
+      <div style={{margin: 12}}>
+        <h2>album desastre ritmo</h2>
+        <iframe  width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1903232455&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+      </div>
     </div>
   );
 };
