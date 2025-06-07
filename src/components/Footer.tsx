a4de41c4df48ad46beec205f624da6d7969d349b
Joao Pilger <joao.pilger@glim.com.br>
2025-06-07 13:21:45 -0300

    v16

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,12 @@ const Footer = () => {
     <footer className="footer">
       <p>&copy; 2025 dj rhonaz - funk e pirataria</p>
       <div className="social-links">
-        <a href="https://instagram.com/rhonaz.pilger" target="_blank" rel="noopener noreferrer">Instagram</a>
-        <a href="https://wa.me/5551995022584?text=oi%20dj%20rhonaz" target="_blank" rel="noopener noreferrer">WhatsApp</a>
         <a href="https://open.spotify.com/intl-pt/artist/7lRJoEeNJA0OdsiJQQHaNX?si=jmtKTmQjQCylfI7rsRZqWQ" target="_blank" rel="noopener noreferrer">Spotify</a>
         <a href="https://soundcloud.com/rhonaz" target="_blank" rel="noopener noreferrer">SoundCloud</a>
         <a href="https://www.youtube.com/@rhonaz/videos" target="_blank" rel="noopener noreferrer">YouTube</a>
         <a href="https://dj-rhonaz.bandcamp.com/" target="_blank" rel="noopener noreferrer">BandCamp</a>
+        <a href="https://instagram.com/rhonaz.pilger" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="https://wa.me/5551995022584?text=oi%20dj%20rhonaz" target="_blank" rel="noopener noreferrer">WhatsApp</a>
       </div>
     </footer>
   );
