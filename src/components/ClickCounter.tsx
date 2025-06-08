b4d6438f28c380351dc9b014e75981d295b72487
Joao Pilger <joao.pilger@glim.com.br>
2025-06-08 17:34:33 -0300

    v18

--- src/components/ClickCounter.tsx
+++ src/components/ClickCounter.tsx
@@ -6,16 +6,16 @@ const ClickCounter: React.FC = () => {
 
   return (
 <>
-    <div style={{display: 'flex', flexDirection: 'row', gap: '10px', position: 'absolute', bottom: '10px', left: '10px'}}>
+    <div style={{display: 'flex', flexDirection: 'row', position: 'absolute', bottom: '10px', left: '10px'}}>
       <img src={som} alt="giphy" style={{width: '100px', height: '170px'}}/>
     </div>
-    <div style={{display: 'flex', flexDirection: 'column', gap: '10px', position: 'absolute', left: '50%', bottom: '10px', transform: 'translateX(-50%)'}}>
+    <div style={{display: 'flex', flexDirection: 'column',position: 'absolute', left: '50%', bottom: '20px', transform: 'translateX(-50%)'}}>
       <img src={som2} alt="cd2" style={{width: '200px', height: '100px'}} />
     </div>
-    <div style={{display: 'flex', flexDirection: 'row', gap: '10px', position: 'absolute', bottom: '10px', right: '10px'}}>
+    <div style={{display: 'flex', flexDirection: 'row',position: 'absolute', bottom: '10px', right: '10px'}}>
       <img src={som} alt="giphy" style={{width: '100px', height: '170px'}}/>
     </div>
-    <div style={{display: 'flex', flexDirection: 'column', gap: '10px', position: 'absolute', left: '50%', bottom: '10px', transform: 'translateX(-50%)'}}>
+    <div style={{display: 'flex', flexDirection: 'column', position: 'absolute', left: '50%', bottom: '120px', transform: 'translateX(-50%)'}}>
       <p>Clique na estrela e ganhe um presente 🎁</p>
     </div>
     </>
